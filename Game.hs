4f55348b07df73d5259504e91331c917e59a1b15
Noah Luck Easterly <noah.easterly@gmail.com>
2011-01-02 19:30:49 -0500

    shifted to using the state monad

--- Game.hs
+++ Game.hs
@@ -1,6 +1,29 @@
-{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE ExistentialQuantification, RankNTypes #-}
 module Clue.Game where
+{-
+ - return
+ -  State Monad :: for StdGen (random number generator)
+ -  Writer Monad :: for game log
+ -  Maybe PlayerPosition :: winner (if any)
+game players =
+  -- choose the true scenario and remove those cards
+  -- deal out the remaining cards to each player
+  -- allow each player to go until one player wins (or all lose)
+    -- on a player's turn, 
+      -- a player may make a suggestion
+        -- other players each get a chance to rebut, if they can
+      -- a player may make an accusation
+        -- if true, they win.
+        -- if false, they may no longer take turns 
+  -- return a log of gameplay
+turn =~ /(suggest pass* reveal?)? accuse?/
+if you try to reveal an impossible card (you don't carry it, it's not relevent),
+you're marked as a cheat
+  - cheats can no longer take turns
+  - cheats don't get to choose which card to reveal
+-}
 import Control.Monad (liftM, unless)
+import Control.Monad.State
 import System.Random (randomRIO)
 import Data.List ((\\), find, intersect)
 
@@ -39,68 +62,45 @@ type TotalPlayers = Int
 type PlayerPosition = Int
 
 
-{-
- - return
- -  State Monad :: for StdGen (random number generator)
- -  Writer Monad :: for game log
- -  Maybe PlayerPosition :: winner (if any)
-game players =
-  -- choose the true scenario and remove those cards
-  -- deal out the remaining cards to each player
-  -- allow each player to go until one player wins (or all lose)
-    -- on a player's turn, 
-      -- a player may make a suggestion
-        -- other players each get a chance to rebut, if they can
-      -- a player may make an accusation
-        -- if true, they win.
-        -- if false, they may no longer take turns 
-  -- return a log of gameplay
-turn =~ /(suggest pass* reveal?)? accuse?/
-if you try to reveal an impossible card (you don't carry it, it's not relevent),
-you're marked as a cheat
-  - cheats can no longer take turns
-  - cheats don't get to choose which card to reveal
--}
-
-data Agent = forall p. Player p => Agent p
-
--- deal this player in
-type MkAgent = TotalPlayers -> PlayerPosition -> [Card] -> IO Agent
-data PlayerInfo = PlayerInfo  { agent :: Agent
-                              , position :: PlayerPosition
-                              , hand :: [Card]
-                              , lost :: Bool
-                              , cheated :: Bool }
 
 data Event  = Suggestion PlayerPosition Scenario
             | Reveal PlayerPosition
             | WinningAccusation PlayerPosition Scenario
             | LosingAccusation PlayerPosition Scenario
 
-class Player a where
+class Player p where
   -- let them know what another player does
-  update :: a -> Event -> IO a
+  update :: Event -> StateT p IO ()
 
   -- ask them to make a suggestion
-  suggest :: a -> IO (a, Maybe Scenario)
+  suggest :: StateT p IO (Maybe Scenario)
 
   -- ask them to make an accusation
-  accuse :: a -> IO (a, Maybe Scenario)
+  accuse :: StateT p IO (Maybe Scenario)
 
   -- ask them to reveal a card to invalidate a suggestion
-  reveal :: a -> (PlayerPosition, Scenario) -> IO (a, Card)
+  reveal :: (PlayerPosition, Scenario) -> StateT p IO Card
 
 
-wrap :: Player a => PlayerInfo -> IO (a, b) -> IO (PlayerInfo, b)
-wrap p m = m >>= \(a,b) -> return (p { agent = Agent a }, b)
+data Agent = forall p. Player p => Agent p
+type MkAgent = TotalPlayers -> PlayerPosition -> [Card] -> IO Agent
+data PlayerInfo = PlayerInfo  { agent :: Agent
+                              , position :: PlayerPosition
+                              , hand :: [Card]
+                              , lost :: Bool
+                              , cheated :: Bool }
+data Action b = Action (forall p. Player p => StateT p IO b)
+wrap :: Action b -> StateT PlayerInfo IO b
+wrap (Action a) = StateT $ \(pi@PlayerInfo { agent = Agent p }) -> do
+  (b, p) <- runStateT a p
+  return $ (b, pi { agent = Agent p })
 
 instance Player PlayerInfo where
-  update  (p@PlayerInfo { agent=(Agent a) }) = wrap p . update a
-    where wrap p m = m >>= \a -> return $ p { agent = Agent a }
-  suggest (p@PlayerInfo { agent=(Agent a) }) = wrap p $ suggest a
-  accuse  (p@PlayerInfo { agent=(Agent a) }) = wrap p $ accuse a
-  reveal  (p@PlayerInfo { agent=(Agent a) }) = wrap p . reveal a
-  
+  update ev = wrap $ Action $ update ev
+  suggest   = wrap $ Action $ suggest
+  accuse    = wrap $ Action $ accuse
+  reveal sg = wrap $ Action $ reveal sg
+ 
 -- draw a random member of a list
 draw :: [a] -> IO (a, [a])
 draw as = do
@@ -122,9 +122,10 @@ deal n [] = replicate n []
 deal n as = zipWith (:) cs $ deal n as'
   where (cs, as') = splitAt n as
 
-apply :: MkAgent -> TotalPlayers -> PlayerPosition -> [Card] -> IO PlayerInfo
-apply m n i cs = m n i cs >>= \a -> return $ PlayerInfo a i cs False False
+create :: MkAgent -> TotalPlayers -> PlayerPosition -> [Card] -> IO PlayerInfo
+create m n i cs = m n i cs >>= \a -> return $ PlayerInfo a i cs False False
 
+{-
 playturn :: TotalPlayers -> [PlayerInfo] -> IO (Maybe PlayerPosition)
 playturn n (p:ps) = do
   -- see if they have a suggestion
@@ -137,6 +138,7 @@ playturn n (p:ps) = do
       let cs = map ($scenario) [ Suspect . getWho, Room . getWhere, Weapon . getHow ] 
       case find (not . null . intersect cs . hand) ps of _ -> return (p:ps) 
   return Nothing
+-}
 
 playgame :: [MkAgent] -> IO (Maybe PlayerPosition)
 playgame ms = do
@@ -148,5 +150,6 @@ playgame ms = do
       (weapon,_)  <- draw weapons
       (room,_)    <- draw rooms
       deck        <- shuffle $ cards \\ [ Suspect killer, Weapon weapon, Room room ]
-      ps          <- sequence $ zipWith3 (flip apply n) ms [0..] (deal n deck)
+      ps          <- sequence $ zipWith3 (flip create n) ms [0..] (deal n deck)
       return Nothing
+
