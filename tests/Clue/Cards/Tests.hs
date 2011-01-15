d606a019dfff2eba17950e62d5e2da106c4b31ba
Noah Luck Easterly <noah.easterly@gmail.com>
2011-01-15 16:02:38 -0500

    slowly adding more tests

--- tests/Clue/Cards/Tests.hs
+++ tests/Clue/Cards/Tests.hs
@@ -3,18 +3,31 @@ import Test.Framework
 
 import Clue.Cards
 
-import Data.List (transpose)
+import Data.List (transpose, sort)
+import System.Random (mkStdGen)
+import Control.Monad.State (runStateT)
+import Control.Monad.Identity (runIdentity)
 
-data Undealt = Undealt Int String
-  deriving Show
-instance Arbitrary Undealt where
-  arbitrary = do
-    deck <- arbitrary
-    n    <- choose (1, 2 * length deck)
-    return $ Undealt n deck
+runShuffle :: Int -> [a] -> [a]
+runShuffle i as = fst $ runIdentity $ shuffle as `runStateT` mkStdGen i
+    
+prop_shuffle_returns_permutation :: Int -> String -> Property
+prop_shuffle_returns_permutation i as =
+  property $ sort as == sort (runShuffle i as)
 
-prop_creates_n_lists :: Undealt -> Property
-prop_creates_n_lists (Undealt n as) = property $ n == length (deal n as)
+prop_shuffle_depends_on_gen :: Int -> String-> Property
+prop_shuffle_depends_on_gen g s = 
+  property $ any id [ runShuffle g' as /= as' | g' <- [0..] ]
+    where as = [ 0 .. length s + 1 ]
+          as' = runShuffle g as
 
-prop_distributes_cards :: Undealt -> Property
-prop_distributes_cards (Undealt n as) = property $ as == concat (transpose $ deal n as)
+forAllDeals :: [a] -> (Int -> Property) -> Property
+forAllDeals s f = forAll (choose (1, 2 * length s)) f
+
+prop_deal_creates_n_lists :: String -> Property
+prop_deal_creates_n_lists s = forAllDeals s $ \n -> 
+  property $ n == length (deal n s)
+
+prop_deal_distributes_cards :: String -> Property
+prop_deal_distributes_cards s = forAllDeals s $ \n -> 
+  property $ s == concat (transpose $ deal n s)
